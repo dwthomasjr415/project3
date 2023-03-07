4c624427ac7764099ab7e05e5a1f1a5c7506b5b7
Charles <cmason4109@gmail.com>
2023-03-07 14:17:55 -0800

    false

--- EKSMOD.tf
+++ EKSMOD.tf
@@ -33,24 +33,24 @@ module "eks" {
     }
   }
   # aws-auth configmap
-  manage_aws_auth_configmap = true
+  manage_aws_auth_configmap = false
 
-  aws_auth_roles = [
-    {
-      rolearn  = "arn:aws:iam::257248662189:role/team1project3-cluster-20230307003005940400000002"
-      username = "Student"
-      groups   = ["system:masters"]
-    }
-  ]
-  aws_auth_accounts = [
-    "257248662189"
-  ]
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:sts::257248662189:assumed-role/AWSReservedSSO_Student_7a53284de0a60a54/cmason4109@gmail.com"
-      username = "admin"
-      groups   = ["system:masters"]
-    }
+  # aws_auth_roles = [
+  #   {
+  #     rolearn  = "arn:aws:iam::257248662189:role/team1project3-cluster-20230307003005940400000002"
+  #     username = "Student"
+  #     groups   = ["system:masters"]
+  #   }
+  # ]
+  # aws_auth_accounts = [
+  #   "257248662189"
+  # ]
+  # aws_auth_users = [
+  #   {
+  #     userarn  = "arn:aws:sts::257248662189:assumed-role/AWSReservedSSO_Student_7a53284de0a60a54/cmason4109@gmail.com"
+  #     username = "admin"
+  #     groups   = ["system:masters"]
+  #   }
   ]
   tags = {
     Owner = "t1-proj3"
