e956a6d54db6ea517ccf1d1ce877a7d5d67d5fce
Charles <cmason4109@gmail.com>
2023-03-06 16:22:48 -0800

    changed eks cluster name

--- EKSMOD.tf
+++ EKSMOD.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name    = "my-cluster"
+  cluster_name    = "team1project3"
   cluster_version = "1.24"
 
   cluster_endpoint_public_access = true
