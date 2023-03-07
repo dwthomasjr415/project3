1ded53bf328bdf98737617965be64eda50ac9bac
Charles <cmason4109@gmail.com>
2023-03-07 14:14:32 -0800

    switched rolearn to team1project3-cluster

--- EKSMOD.tf
+++ EKSMOD.tf
@@ -37,8 +37,8 @@ module "eks" {
 
   aws_auth_roles = [
     {
-      rolearn  = "arn:aws:iam::257248662189:user/dt-proj1"
-      username = "dt-proj1"
+      rolearn  = "arn:aws:iam::257248662189:role/team1project3-cluster-20230307003005940400000002"
+      username = "Student"
       groups   = ["system:masters"]
     }
   ]
@@ -47,7 +47,7 @@ module "eks" {
   ]
   aws_auth_users = [
     {
-      userarn  = "arn:aws:sts::257248662189:assumed-role/AWSReservedSSO_Student_7a53284de0a60a54/*"
+      userarn  = "arn:aws:sts::257248662189:assumed-role/AWSReservedSSO_Student_7a53284de0a60a54/cmason4109@gmail.com"
       username = "admin"
       groups   = ["system:masters"]
     }
