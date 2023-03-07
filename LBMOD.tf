859745cbd001f46ff12f50003faf8fc281d72f8d
Charles <cmason4109@gmail.com>
2023-03-07 09:19:43 -0800

    added LBMOD

---
+++ LBMOD.tf
@@ -0,0 +1,28 @@
+module "elb_http" {
+  source  = "terraform-aws-modules/elb/aws"
+  version = "~> 2.0"
+
+  name = "t1-proj3-elb"
+
+  subnets         = [var.t1-proj3pubsubs1, var.t1-proj3pubsubs2]
+  security_groups = ["*"]
+  internal        = false
+
+  listener = [
+    {
+      instance_port     = 80
+      instance_protocol = "HTTP"
+      lb_port           = 80
+      lb_protocol       = "HTTP"
+    },
+
+  ]
+
+  health_check = {
+    target              = "HTTP:80/"
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+  }
+}
--- Variables.tf
+++ Variables.tf
@@ -5,6 +5,15 @@ variable "t1-proj3prisubs1" {
 
 variable "t1-proj3prisubs2" {
   type    = string
-  default = "t1-proj3prisubse2"
+  default = "t1-proj3prisubs2"
 }
 
+variable "t1-proj3pubsubs1" {
+  type    = string
+  default = "t1-proj3pubsubs1"
+}
+
+variable "t1-proj3pubsubs2" {
+  type    = string
+  default = "t1-proj3pubsubs2"
+}
