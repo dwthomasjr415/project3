735f8de86bddd18deb9f48a4bc0c5d0ca149456a
dwthomasjr415 <dwthomasjr@gmail.com>
2023-03-09 13:54:17 -0500

    Removed the test subnets

--- VPCMOD.tf
+++ VPCMOD.tf
@@ -3,8 +3,8 @@ module "vpc" {
   name                   = "t1-proj3VPC"
   cidr                   = "10.0.0.0/16"
   azs                    = ["us-east-1a", "us-east-1b"]
-  private_subnets        = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets         = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets        = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets         = ["10.0.101.0/24", "10.0.102.0/24"]
   enable_nat_gateway     = true
   single_nat_gateway     = false
   one_nat_gateway_per_az = true
